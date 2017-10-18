577830f61c415b42999ccea7d41058bf75c3ed16
Clare Sudbery <claresudbery@users.noreply.github.com>
2017-10-18 15:40:08 +0100

    Get rid of unused instructions const

--- jeopardyTutorial/src/App.js
+++ jeopardyTutorial/src/App.js
@@ -19,13 +19,6 @@ import Ember from './Ember';
 import ReactJS from './React';
 import Vue from './Vue';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
 const Container = styled.View`
   justifyContent: center;
   alignItems: center;
