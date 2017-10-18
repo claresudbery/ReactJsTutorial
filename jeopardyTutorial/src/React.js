1659028b77dba0e74430758a088b5e4933bc0348
Clare Sudbery <claresudbery@users.noreply.github.com>
2017-10-18 16:45:37 +0100

    Removed unused instructions constant

--- jeopardyTutorial/src/React.js
+++ jeopardyTutorial/src/React.js
@@ -13,13 +13,6 @@ import {
 } from 'react-native';
 import styled from 'styled-components/native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
 const Container = styled.View`
   justifyContent: center;
   alignItems: center;
