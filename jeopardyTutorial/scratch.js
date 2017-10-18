a40d9dcb51074cbca3399a3cafae33065599c8c7
Clare Sudbery <claresudbery@users.noreply.github.com>
2017-10-18 17:43:21 +0100

    A simpler attempt to use ListItem

--- jeopardyTutorial/scratch.js
+++ jeopardyTutorial/scratch.js
@@ -1,1 +1,15 @@
 // general gubbins goes here...
+
+
+        <List containerStyle={{marginBottom: 20}}>
+          {
+            data.map(trivia => (
+              <ListItem
+                roundAvatar
+                avatar={{uri:l.avatar_url}}
+                key={i}
+                title={l.name}
+              />
+            ))
+          }
+        </List>
--- jeopardyTutorial/src/React.js
+++ jeopardyTutorial/src/React.js
@@ -6,46 +6,54 @@
 
 import React, { Component } from 'react';
 import {
+  AppRegistry,
   StyleSheet,
   Text,
   View,
-  List,
-  ListItem,
-  ScrollView,
-  Image
+  Image,
+  ScrollView
 } from 'react-native';
 import styled from 'styled-components/native';
-import Question from './Question';
-import Answer from './Answer';
+import {List, ListItem} from 'react-native-elements';
 
 const data = [{
+  id: 1,
   Q: "The company that created React.",
   A: "What is Facebook?"
 },{
+  id: 1,
   Q: "What happens during the lifecycle of a React component?",
   A: "Initialization, State/property updates, Destruction"
 },{
+  id: 1,
   Q: "True or False: JSX a requirement in React.",
   A: "What is false?"
 },{
+  id: 1,
   Q: "A popular flux framework.",
   A: "What is Redux, Flummox, Alt, Fluxible, etc?"
 },{
+  id: 1,
   Q: "A component that is a function of the properties provided to it.",
   A: "What are stateless components?"
 },{
+  id: 1,
   Q: "True or False: React is an MVC based framework.",
   A: "What is false?"
 },{
+  id: 1,
   Q: "_____ needs to be updated to achieve dynamic UI updates.",
   A: "What is state?"
 },{
+  id: 1,
   Q: "The API necessary for every React component.",
   A: "What is render?"
 },{
+  id: 1,
   Q: "React focuses on this part of MVC.",
   A: "What is view?"
 },{
+  id: 1,
   Q: "The name of the CLI used to create react apps.",
   A: "What is create-react-app?"
 }];
@@ -70,18 +78,12 @@ export default class ReactJS extends Component<{}> {
           ReactJS Jeopardy
         </TextStyled>
         <List>
-          {data.map(trivia =>
-            <ListItem key={trivia.Q}>
-              <List>
-                <ListItem>
-                  {trivia.Q}
-                </ListItem>
-                <ListItem>
-                  {trivia.A}
-                </ListItem>
-              </List>
-            </ListItem>
-          )}
+          {data.map((trivia) => (
+            <ListItem 
+              key={trivia.Q}
+              title={trivia.Q}
+            />
+          ))}
         </List>
       </Container>
     );
